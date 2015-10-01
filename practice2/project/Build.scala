02adbffa066030ace2d84ec9325fde12d5466193
Pun, Ka Shun Carson <pun.ka.shun@gmail.com>
2015-09-30 17:11:21 -0700

    added a more complicated GUI example

--- practice2/project/Build.scala
+++ practice2/project/Build.scala
@@ -5,7 +5,7 @@ object MyBuild extends Build {
 
   lazy val root = project.in(file(".")).
                   settings(commonSettings: _*).
-                  dependsOn(ex1,ex2)
+                  dependsOn(ex1,ex2,ex3)
 
   lazy val ex1 = project.in(file("ex1")).
                   settings(commonSettings: _*).
@@ -25,6 +25,14 @@ object MyBuild extends Build {
                     )
                   )
 
+  lazy val ex3 = project.in(file("ex3")).
+                  settings(commonSettings: _*).
+                  settings(
+                    libraryDependencies ++= Seq(
+                      "org.scalatest" % "scalatest_2.11" % "2.2.4",
+                      "org.scala-lang" % "scala-swing" % "2.11.0-M7"
+                    )
+                  )
 
   def commonSettings = 
     Seq(
